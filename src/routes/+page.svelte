4e5d1366da3e4801387232a61a88a33540aff803
Mikki Friend <realmikkifriend@gmail.com>
2025-06-02 10:41:36 -0500

    chore: updated app name in code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
 
 <div class="flex justify-center items-start min-h-screen bg-primary mx-2">
 	<div class="max-w-xl w-full bg-secondary p-6 rounded-lg shadow-md mt-8 relative">
-		<h1 class="mb-4">Pocket Stats</h1>
+		<h1 class="mb-4">Reading List Stats</h1>
 
 		<select
 			bind:value={theme}
@@ -45,11 +45,11 @@
 		{#if typeof token !== 'string' || token.length === 0}
 			<div class="flex flex-col sm:flex-row items-center">
 				<button class="mr-2 my-1 h-fit">
-					<a href={authUri} class="button whitespace-nowrap">Authorize Pocket</a>
+					<a href={authUri} class="button whitespace-nowrap">Authorize to Access Reading List</a>
 				</button>
 				<p class="text-center sm:text-left">
-					This mini-app will grab a copy of your current <a href="https://getpocket.com/">Pocket</a>
-					reading list and then display some stats and charts.
+					This mini-app will grab a copy of your current reading list and then display some stats
+					and charts.
 				</p>
 			</div>
 		{:else if list.length > 0}
@@ -61,9 +61,9 @@
 		{/if}
 
 		<footer>
-			<a href="https://github.com/realmikkifriend/pocket-stats">open-source app</a> created by
+			<a href="https://github.com/realmikkifriend/readinglist-stats">open-source app</a> created by
 			<a href="https://github.com/realmikkifriend/">Mikki Friend</a><br />
-			not endorsed by Pocket &bull; no personal data is stored
+			not endorsed by any app &bull; no personal data is stored
 		</footer>
 	</div>
 </div>
